7bb40dfa0ee77c35c49e8a62b1086785e95fa09e
davidmm1011 <57577144+davidmm1011@users.noreply.github.com>
2020-05-01 14:40:08 -0400

    Delete instalarch4.sh

--- instalarch4.sh
+++
@@ -1,9 +0,0 @@
-echo LANG=es_BO.UTF-8 > /etc/locale.conf
-locale-gen
-hwclock -w
-echo KEYMAP=la-latin1 > /etc/vconsole.conf
-grub-install /dev/sda
-grub-mkconfig -o /boot/grub/grub.cfg
-os-prober
-grub-mkconfig -o /boot/grub/grub.cfg
-passwd
